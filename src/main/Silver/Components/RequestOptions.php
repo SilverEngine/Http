65e652588e1d293a18bf8550dd7295e33d0ca3b4
Nicolás Marulanda P <nicomaru@outlook.com>
2019-02-17 14:48:31 +0100

    Request options - [Add] Clase RequestOption. List of options to customize the requests created by the client.

---
+++ src/main/Silver/Components/RequestOptions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * RequestOptions.php
+ */
+
+namespace Silver\Components;
+
+/**
+ * Class RequestOptions
+ * @author Nicolás Marulanda P.
+ */
+class RequestOptions {
+    
+    const CURL    = 'curl';
+    
+    const QUERY   = 'query';
+    
+    const JSON    = 'json';
+    
+    const HEADERS = 'headers';
+    
+    const TIMEOUT = 'timeout';
+    
+}
